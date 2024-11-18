c31561bfb203f5b14aa63364eba6c5ce29580948
Sriaditya-S <98731541+Sriaditya-S@users.noreply.github.com>
2024-11-18 16:32:34 +0530

    Add files via upload

---
+++ onlinecourse/onlinecourse/admin/includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                     VIT Online Course Registration Portal 
+                </div>
+
+            </div>
+        </div>
+    </footer>
